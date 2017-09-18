eca378eb5ecebe4e6480c7c57c75031698956972
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-09-18 22:33:45 +0530

    optimized the generic CBV

--- rest/snippets/views.py
+++ rest/snippets/views.py
@@ -1,12 +1,12 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetList(ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class SnippetDetail(RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
