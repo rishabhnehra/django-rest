73c67cbe3f2f16dd8187cb040c9786347a73d10a
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-09-18 22:31:31 +0530

    Used generic CBV

--- rest/snippets/views.py
+++ rest/snippets/views.py
@@ -1,48 +1,12 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 
-class SnippetList(APIView):
-
-	def get(self, request, format = None):
-		snippets = Snippet.objects.all()
-		serializer = SnippetSerializer(snippets, many = True)
-		return Response(serializer.data)
-
-	def post(self, request, format = None):
-		serializer = SnippetSerializer(data = request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status = status.HTTP_201_CREATED)
-		return Response(serializer.error, status = status.HTTP_400_BAD_REQUEST)
+class SnippetList(generics.ListCreateAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
 
 
-class SnippetDetail(APIView):
-
-	def get_object(self, pk):
-		try:
-			return Snippet.objects.get(pk = pk)
-		except Snippet.DoesNotExist: 
-			raise Http404
-
-	def get(self, request, pk, format = None):
-		snippet = self.get_object(pk = pk)
-		serializer = SnippetSerializer(snippet)
-		return Response(serializer.data)
-
-	def put(self, request, pk, format = None):
-		snippet = self.get_object(pk = pk)
-		serializer = SnippetSerializer(snippet)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.error, status = status.HTTP_400_BAD_REQUEST)
-
-
-	def delete(self, request, pk, format = None):
-		snippet = Snippet.objects.get(pk = pk)
-		snippet.delete()
-		return Response(status = status.HTTP_204_NO_CONTENT)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
